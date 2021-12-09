25d497998dd83afc02890a478fac97d48e3fd492
Frank Albanese <albanesefc9@gmail.com>
2021-12-09 18:00:49 -0500

    Multiple Routes Vid - 18:00

--- market.py
+++ market.py
@@ -1,7 +1,12 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 
-@app.rout('/')
+@app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>HELLO WORLD!!</h>'
+
+@app.route('/about')
+def about_page():
+    return '<h1>About Page</h>'
+
 
