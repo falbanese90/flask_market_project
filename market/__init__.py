8ad28bae472802908e08dbcac5d0acab940e8589
Frank Albanese <albanesefc9@gmail.com>
2021-12-11 21:24:31 -0500

    Passwords hash is saved in database Vid- 3:49:44

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'feaf40092d92632b62b75234'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
