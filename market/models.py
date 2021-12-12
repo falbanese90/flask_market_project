7078c2e87aac2b5535bcaa21ad656a6c59f9c998
Frank Albanese <albanesefc9@gmail.com>
2021-12-11 22:40:33 -0500

    Login is functional Vid- 4:18:56

--- market/models.py
+++ market/models.py
@@ -1,5 +1,10 @@
-from market import db
+from market import db, login_manager
 from market import bcrypt
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -12,7 +17,7 @@ class Item(db.Model):
     def __repr__(self):
         return f'Item: {self.name}'
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
@@ -27,4 +32,8 @@ class User(db.Model):
     @password.setter
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
     
